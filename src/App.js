c1d04c33939c6805a2ff16d7a241303e81e4b9f2
denistorjai <denistorjai@outlook.com>
2024-03-20 01:46:12 -0600

    Added page routing

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
-import './App.css';
-import './Styles/Mobile.css'
-import './Styles/Hero.css'
-import './Styles/Navbar.css'
-import './Styles/Footer.css'
-import './Styles/4k.css'
-
-import Navbar from './Components/navbar'
-import Footer from './Components/footer'
-import HeroVideo from './Components/herovideo'
-import HeroText from './Components/herotext';
-
-import bannervideo from './Assets/tempherovideo.mp4';
+import Main from './Pages/main'
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
 
 function App() {
 
   return (
 
-    <div className="App">
-      <header className="App-header"></header>
-      <body>
-        <Navbar/>
-
-        {/* Hero */}
-        <HeroText Title1={"Introducing Wolfgang:"} Title2={"our latest product that provides relief"} SubTitle={"New AI powered anticheat that excels in both prevention and catching cheaters in action"} ButtonText={"Request a Demo"} />
-        <HeroVideo VideoPath={bannervideo} />
-
-        {/* Page Body Code Below */}
-
-    
-
-      </body>
-      <Footer/>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Main/>} />
+      </Routes>
+    </Router>
 
   );
 }
