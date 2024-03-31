3f5e575710b513fdd467e7f9bb48e85da0c745a0
denistorjai <denistorjai@outlook.com>
2024-03-30 23:02:50 -0600

    Update herovideo.js

--- src/Components/herovideo.js
+++ src/Components/herovideo.js
@@ -1,14 +1,11 @@
 function HeroVideo( { VideoPath } ) {
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
   return (
-
     <div className="HeroContainer">
-
-        <video className="HeroVideo" muted loop autoPlay>
+      <video className="HeroVideo" muted loop autoPlay={true} {...(isMobile && { playsInline: true })}>
         <source src={VideoPath} type="video/mp4" />
-        </video>
-
+      </video>
     </div>
-
   );
 }
 
